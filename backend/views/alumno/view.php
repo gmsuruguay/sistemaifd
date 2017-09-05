bd8fe049f98f552f19d4de77608c102259bbdc92
tinchohlj <tinchohlj@gmail.com>
2017-09-05 12:35:33 -0300

    se agrego pestania al tabs de alumnos

--- backend/views/alumno/view.php
+++ backend/views/alumno/view.php
@@ -13,12 +13,17 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="alumno-view">
 
     <div class="nav-tabs-custom">
-        <ul class="app-icono nav nav-tabs pull-right">          
-          <li class=""><a href="#datos_academicos" data-toggle="tab" aria-expanded="false"><i class="fa  fa-briefcase"></i> Datos Academicos</a></li>
+        <ul class="app-icono nav nav-tabs pull-right">  
+        <li class=""><a href="#cursada" data-toggle="tab" aria-expanded="false"><i class="fa  fa-book"></i> Mis Cursadas</a></li>        
+          <li class=""><a href="#datos_academicos" data-toggle="tab" aria-expanded="false"><i class="fa  fa-graduation-cap"></i> Datos Academicos</a></li>
           <li class="active"><a href="#legajo" data-toggle="tab" aria-expanded="true"><i class="fa fa-file-text"></i> Datos Personales</a></li>          
           <li class="pull-left header "><i class="fa fa-user"></i> <?=$model->nombreCompleto?></li>
         </ul>
-        <div class="tab-content">              
+        <div class="tab-content">   
+          <!-- /.tab-pane -->
+          <div class="tab-pane" id="cursada">
+            
+          </div>           
           <!-- /.tab-pane -->
           <div class="tab-pane" id="datos_academicos">
             <?php  echo $this->render('_academico', ['model'=>$model,'searchModel' => $searchModel,'dataProvider' => $dataProvider]); ?>

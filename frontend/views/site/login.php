cbe3727648d1666a3329f67f712b3233efbfcfae
tinchohlj <tinchohlj@gmail.com>
2017-10-19 21:11:53 -0300

    se aplico estilos a la pagina de login

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -7,33 +7,36 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Bienvenido a SURI';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login ">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
+    
 
     <div class="row">
         <div class="col-lg-5">
+          <div class="box-login">
+            <div class="box-body">
+                <h3 class="text-center">Iniciar Sesión</h3>
+            </div>
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Usuario') ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                <?= $form->field($model, 'password')->passwordInput()->label('Contraseña') ?>
+               
 
                 <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+                     <?= Html::a('¿Olvidaste tu contraseña?', ['site/request-password-reset']) ?>.
                 </div>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Login', ['class' => 'btn btn-block btn-info', 'name' => 'login-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
+          </div>
+            
         </div>
     </div>
 </div>

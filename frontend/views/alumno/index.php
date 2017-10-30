76f4d35992741ec89dc4e684415a9d63c6d1d4ea
tinchohlj <tinchohlj@gmail.com>
2017-10-30 13:43:02 -0300

    se agrego estilos a la vista index

--- frontend/views/alumno/index.php
+++ frontend/views/alumno/index.php
@@ -4,6 +4,7 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 use common\models\FechaHelper;
 use yii\bootstrap\ButtonDropdown;
+use mdm\admin\components\Helper;
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\search\InscripcionSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -12,75 +13,33 @@ $this->title = 'Carreras Inscriptas';
 ?>
 <div class="inscripcion-index">
     
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,        
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            [
-				'attribute'=>'carrera_id',
-				'label'=>'Carrera',
-				'format'=>'text',//raw, html
-				'content'=>function($data){
-					return $data->descripcionCarrera;
-					}
-			],           
-            
-            'nro_libreta',
-            [
-            'attribute'=>'fecha',
-            'label'=>'Fecha Inscripción',
-            'format'=>'text',//raw, html
-            'content'=>function ($data){
-                return FechaHelper::fechaDMY($data->fecha);
-            }
-            ], 
-            ['class' => 'yii\grid\ActionColumn', 'template' => '{inscribir} {inscripcion}', 
-            
-            'buttons' => [
-
-                'inscribir'=> function ($url, $model, $key) {
-                    return ButtonDropdown::widget([
-                        'encodeLabel' => false, // if you're going to use html on the button label
-                        'label' => '<span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Inscribir',
-                        'dropdown' => [
-                            'encodeLabels' => false, // if you're going to use html on the items' labels
-                            'items' => [
-                                [
-                                    'label' => 'Materia',
-                                    'url' => ['listar-materia', 'id' => $model->carrera_id],                                                                       
-                                ],
-                                [
-                                    'label' => 'Exámen',
-                                    'url' => ['#'],                                    
-                                    'visible' => true,  // if you want to hide an item based on a condition, use this
-                                ],
-                                
-                            ],
-                            'options' => [
-                                'class' => 'dropdown-menu-right', // right dropdown
-                            ],
-                        ],
-                        'options' => [
-                            'class' => 'btn btn-primary',   // btn-success, btn-info, et cetera
-                        ],
-                        'split' => true,    // if you want a split button
-                    ]);
-                },  
-                
-                'inscripcion'=> function ($url, $model, $key) {
-                    return Html::a('<span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> Mis inscripciones', ['ver-inscripciones', 'id' => $key], [
-                        'class' => 'btn btn-primary',                        
-                        'title'=>'inscribir en materia'
-                    ]);
-                },
-
-
-            ]],             
-        ],
-    ]); ?>
+    <h3><?= Html::encode($this->title) ?></h3>
+    <?php foreach ($model as $m): ?>
+        <div class="row">
+            <div class="col s12 m12">
+                <div class="card">
+                <div class="card-content">
+                    <span class="card-title"><?=$m->descripcionCarrera ?></span>
+                    <p></p>
+                </div>
+                <div class="card-action">
+                    <?php 
+                    if (Helper::checkRoute('listar-materia')) {
+                        echo Html::a(Yii::t('app', '<i class="material-icons left">create</i> Inscribir materia'), ['listar-materia', 'id' => $m->carrera_id], [
+                            'class' => 'btn waves-effect waves-light'                            
+                        ]);
+                    }  
+                    if(Helper::checkRoute('ver-inscripciones')){
+                        echo Html::a('<i class="material-icons left">format_list_bulleted</i> Mis inscripciones', ['ver-inscripciones', 'id' => $m->id], [
+                            'class' => 'btn waves-effect waves-light',                  
+                            
+                        ]);
+                    }
+                    ?>
+                </div>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
 </div>
             

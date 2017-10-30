b5820b8945da63f2d5e6d9492c57b1b86f71b4d1
tinchohlj <tinchohlj@gmail.com>
2017-10-30 11:06:49 -0300

    formulario login parcialmente modificado

--- frontend/views/layouts/login.php
+++ frontend/views/layouts/login.php
@@ -15,6 +15,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -10,31 +10,19 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Bienvenido a SURI';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login ">
-    
+<div class="site-login ">    
 
     <div class="row">
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col m6 offset-m3">
           <div class="box-login">
-            <div class="box-body">
-                <h3 class="text-center">Iniciar Sesión</h3>
+            <div class="center-align">                
+                <i class="large material-icons">account_circle</i>
             </div>
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Usuario') ?>
-
-                <?= $form->field($model, 'password')->passwordInput()->label('Contraseña') ?>
-               
-
-                <div style="color:#999;margin:1em 0">
-                     <?= Html::a('¿Olvidaste tu contraseña?', ['site/request-password-reset']) ?>.
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-block btn-info', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+            <div class="row">
+                
+            </div>        
+            
           </div>
             
         </div>

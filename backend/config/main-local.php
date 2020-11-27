647ea51c5c869867050ac5ac28113464c1745649
martin_dev <gmsuruguay@gmail.com>
2020-11-27 19:57:01 -0300

    se elimino carpeta vendor

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -4,11 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-<<<<<<< HEAD
-            'cookieValidationKey' => 'WTHHGqVZEI8oM88m4S9YAwXsRujnDKkl',
-=======
             'cookieValidationKey' => 'EFuMuWZ2NKBZX9UU-KixkSnIbH5nHoKs',
->>>>>>> desarrollo
         ],
     ],
 ];
